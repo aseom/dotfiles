1ff2f370a47a162669142bcc4060a784cd85acac
aseom <hm9599@gmail.com>
2016-03-04 02:48:06 +0900

    ST3: SublimeREPL의 Ruby Pry 오류 해결, 사이드바 설정

---
+++ sublime/pry_repl_fixed.rb
@@ -0,0 +1,76 @@
+# Fix ruby Pry helper for Pry version >= 0.10.0.
+# https://github.com/smiler/SublimeREPL/commit/95ffe15382336ef6720502752e71c290055cf7f8
+
+require 'rubygems'
+gem 'pry'
+require 'pry'
+require 'pry/input_completer'
+require 'socket'
+require 'thread'
+require 'json'
+
+include Socket::Constants
+
+class PryInput
+    def readline(prompt)
+        $stdout.print prompt
+        $stdout.flush
+        $stdin.readline
+    end
+end
+
+class PryOutput
+    def print(data="")
+        $stdout.puts(data.gsub('`', "'"))
+        $stdout.flush
+    end
+end
+
+Pry.config.input = PryInput.new()
+Pry.config.output = PryOutput.new()
+Pry.config.color = false
+Pry.config.editor = ARGV[0]
+Pry.config.auto_indent = false
+Pry.config.correct_indent = false
+
+port = ENV["SUBLIMEREPL_AC_PORT"].to_i
+
+socket = Socket.new(AF_INET, SOCK_STREAM, 0)
+sockaddr = Socket.pack_sockaddr_in(port, '127.0.0.1')
+socket.connect(sockaddr)
+completer = Pry::InputCompleter.new(binding)
+
+def read_netstring(s)
+    size = 0
+    while true
+        ch = s.recvfrom(1)[0]
+        if ch == ':'
+            break
+        end
+        size = size * 10 + ch.to_i
+    end
+    msg = ""
+    while size != 0
+        msg += s.recvfrom(size)[0]
+        size -= msg.length
+    end
+    ch = s.recvfrom(1)[0]
+    return msg
+end
+
+# Thread.abort_on_exception = true
+t1 = Thread.new do
+    while true
+        data = read_netstring(socket)
+        req = JSON.parse(data)
+        line = req["line"]
+        completions = completer.call(req["line"])
+        response = [line, completions]
+        response_msg = JSON.dump(response)
+        payload = response_msg.length.to_s + ":" + response_msg + ","
+        socket.write(payload)
+    end
+end
+
+
+Pry.start self
